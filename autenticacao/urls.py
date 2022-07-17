b94a0a4addaae6a87e2d3318ad8c84f4c8582fab
Cassio Rovadoscki Alves <cassioalves@glf.com.br>
2022-07-17 19:29:53 -0300

    Criando aplicação Django sessions

--- autenticacao/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('listar', views.listar, name='listar'),
-    path('listar/<int:id_pessoa>', views.listar_unico, name='listar_unico')  # url dinâmica
-]
